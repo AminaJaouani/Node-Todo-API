87cc4ec72be5a40808ccfa51b69a1defafae3378
amina <aminajaouani@icloud.com>
2025-10-06 10:10:46 +0200

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -96,6 +96,20 @@ app.patch("/todos/:id", (req,res)=>{
     })
 })
 
+// POST /user
+app.post('/users', (req,res)=>{
+    var user = new User( _.pick(req.body, ['email', 'password']))
+    
+    user.save().then(()=>{
+        return user.generateAuthToken()
+    }).then((token)=>{
+        res.header('x-auth',token).send(user)
+    })
+    .catch((e)=>{
+        res.status(400).send()
+    })
+})
+
 app.listen(port, ()=>{
     console.log(`Starting on port ${port}`)
 })
